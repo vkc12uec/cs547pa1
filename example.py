f666d193d4c8bb2175595e829991bdbba090514d
vivekchaurasiya <vivekchaurasiya@gmail.com>
2011-04-18 17:10:42 -0400

    added more code

--- example.py
+++ example.py
@@ -1,10 +1,38 @@
 from nltk.corpus import wordnet as wn
 from pprint import pprint
-hyp = lambda s:s.hypernyms() # useful to print hypernym tree
-line = wn.synset('line.n.3')
-print line.name + ':', line.definition
-pprint(line.tree(hyp))
 
-for pentagon_sense in wn.synsets('pentagon', 'n'):
-	print pentagon_sense.name + ':', pentagon_sense.definition
-	pprint(pentagon_sense.tree(hyp))
+def ex2():
+	import sys
+	right = wn.synset('resident.n.01')
+	left = wn.synset('house_physician.n.01')
+	print right.lowest_common_hypernyms(left)
+	sys.exit()
+
+	synsets = wn.synsets('resident', pos=wn.NOUN)
+	#synsets = wn.synsets('resident')
+	print [ str(syns.definition) for syns in synsets ]
+	#print [ str(syns.hypernyms) for syns in synsets ]
+
+def ex4():
+	right = wn.synset('inhabitant.n.01')
+	left = wn.synset('house_physician.n.01')
+	print right.common_hypernyms(left)
+
+def ex3():
+	from nltk.chat import eliza
+	eliza.eliza_chat()
+
+def ex1():
+	hyp = lambda s:s.hypernyms() # useful to print hypernym tree
+	line = wn.synset('line.n.3')
+	print line.name + ':', line.definition
+	pprint(line.tree(hyp))
+
+	for pentagon_sense in wn.synsets('pentagon', 'n'):
+		print pentagon_sense.name + ':', pentagon_sense.definition
+		pprint(pentagon_sense.tree(hyp))
+
+if __name__ == '__main__':
+	ex4()
+	#ex3()
+	#ex2()
